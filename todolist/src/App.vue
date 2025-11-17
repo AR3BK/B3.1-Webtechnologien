2416f6897a45e554db8336b19afda1d93838ecde
AR3BK <Berkeari2003@hotmail.de>
2025-11-17 13:03:21 +0100

    Complete M2: Add Vue frontend with TodoList component

---
+++ todolist/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <h1>Todo App</h1>
+    <TodoList :todos="todoItems" />
+  </main>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue';
+
+export default {
+  components: { TodoList },
+  data() {
+    return {
+      todoItems: [
+        "Einkaufen",
+        "Lernen",
+        "Hausaufgaben",
+        "In die kluft"
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+main {
+  padding: 20px;
+  font-family: sans-serif;
+}
+</style>
---
+++ todolist/src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Meine Todos</h2>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  props: {
+    todos: Array
+  }
+};
+</script>
