d88f2649548bce4f414dc9c1131c7252586e6636
AR3BK <Berkeari2003@hotmail.de>
2025-12-01 17:22:59 +0100

    frontend access auf backend

--- src/main/java/com/example/demo/AufgabeController.java
+++ src/main/java/com/example/demo/AufgabeController.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin( origins = {"http://localhost:3000", "https://b3-1-webtechnologien-1.onrender.com" } )
 public class AufgabeController {
 
     @GetMapping("/aufgaben")
